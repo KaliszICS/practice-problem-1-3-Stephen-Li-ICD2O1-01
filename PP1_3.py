aea0d5ff406bfb63017dc1a7d6cbf656b609d967
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2024-09-17 14:04:55 +0000

    commit

--- PP1_3.py
+++ PP1_3.py
@@ -1,16 +1,15 @@
 
-
 def q1():
-  #Write Assignment code here
-  #Remember your code must be indented once
+  print(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *")
+  print("\n* * * * *\n*       *\n*       *\n*       *\n* * * * *")
+  print("\n* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *")
 
 def q2():
-  #Write Assignment code here
-  
-def q3():
-  #Write Assignment code here
-  
+  print("Hello ", end = "")
+  print("World\n")
 
+def q3():
+  print('"Man"\n"Board"\n"Man Overboard"')
 
 #Do not alter anything after this line
 q1()
